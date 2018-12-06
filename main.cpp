867682b7f5671ee24621d6bd28d29b32dd9e9009
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2018-12-06 09:39:23 -0500

    Add runtime for each day in output

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 #include "solvers/day1.h"
 #include "solvers/day2.h"
 #include "solvers/day3.h"
+#include <chrono>
+
+using namespace std::chrono;
+
+template<typename F>
+auto timeCallVoid(F f){
+
+    auto pre = high_resolution_clock::now();
+    f();
+    auto post = high_resolution_clock::now();
+
+    return post - pre;
+};
+
+template<typename T>
+void outputDuration(T d) {
+    std::cout << "Duration : " << duration_cast<milliseconds>(d).count() << "ms\n";
+}
 
 int main(){
-    day1::solve();
-    day2::solve();
-    day3::solve();
+    outputDuration(timeCallVoid(day1::solve));
+    outputDuration(timeCallVoid(day2::solve));
+    outputDuration(timeCallVoid(day3::solve));
 }
